cb0a77dc99c8c29cb2773a2cd49abfd49aaba3e3
Nemesix493 <verquerre.kevin@gmail.com>
2023-11-06 11:56:27 +0100

    feat : display projects from database

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from project.models import Project
+
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'home/home_page.html', {'page_title': 'Home Page'})
+    projects = Project.objects.all()
+    return render(
+        request,
+        'home/home_page.html',
+        {
+            'page_title': 'Home Page',
+            'projects': projects
+        }
+    )
