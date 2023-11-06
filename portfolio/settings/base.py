3f6d55eaec267b74a5965c19838257eb9d5e9a39
Nemesix493 <verquerre.kevin@gmail.com>
2023-11-06 11:27:22 +0100

    feat : add custom slug for admin site

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'project'
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,5 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ADMIN_SLUG = os.getenv('ADMIN_SLUG', 'admin')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 from home import views as home_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{settings.ADMIN_SLUG}/', admin.site.urls),
     path('', home_views.home_page, name='home-page')
 ]
